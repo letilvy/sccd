63e3da384003481e4db170696d28fe0498dd9b51
Xiaocheng Wei <xiaocheng.wei@sap.com>
2018-01-24 17:45:29 +0800

    Add tree sample on menu page

--- webapp/controller/Menu.controller.js
+++ webapp/controller/Menu.controller.js
@@ -1,9 +1,13 @@
 sap.ui.define([
-	"sap/support/sccd/controller/BaseController"
-], function(BaseController){
+	"sap/support/sccd/controller/BaseController",
+	"sap/ui/model/json/JSONModel"
+], function(BaseController, JSONModel){
 	"use strict",
 
 	BaseController.extend("sap.support.sccd.controller.Menu", {
-
+		onInit: function(){
+			var oMenuModel = JSONModel(jQuery.sap.getModulePath("sap.support.sccd.model", "/menu.json"));
+			this.getView().setModel(oMenuModel, "menu");
+		}
 	});
 });
