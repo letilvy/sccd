728b8acd43627073d9d1c679885d80a163ba1d72
Xiaocheng Wei <xiaocheng.wei@sap.com>
2018-01-26 19:38:48 +0800

    Add all 5 header kpi

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -14,12 +14,52 @@ sap.ui.define([
 					{ProjectName: "SaE", TotalCase: 491},
 					{ProjectName: "B1 SMP PUM", TotalCase: 259},
 					{ProjectName: "Note Search", TotalCase: 91}
-				]
+				],
+				HealthyUTProjects: 20,
+				HealthyITProjects: 16,
+				"fillLevel": {
+					"threshold": 20,
+					"measure": "lb",
+					"valueBegin": "30",
+					"valueEnd": "5",
+					"timeBegin": "Aug",
+					"timeEnd": "Nov",
+					"color": {
+						"above": "Good",
+						"below": "Error"
+					},
+					"timeSeries": [
+						{
+							"time": 0,
+							"level": 30
+						},
+						{
+							"time": 1,
+							"level": 49
+						},
+						{
+							"time": 2,
+							"level": 28
+						},
+						{
+							"time": 2.5,
+							"level": 11
+						},
+						{
+							"time": 3,
+							"level": 5
+						}
+					]
+				}
 			}), "kpi");
 		},
 
 		onLoadHome: function(){
 			
+		},
+
+		getValuesDelta: function(fFirstValue, fSecondValue) {
+			return fSecondValue - fFirstValue;
 		}
 	});
 });
